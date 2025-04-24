8f4312a3cd339fce88d668dd7c3560a2a9e6f69a
Yuto1114 <endoyuto1114840@gmail.com>
2025-04-24 12:12:05 +0900

    学生更新の修正

--- exam_login/WebContent/scoremanager/main/student_update_done.jsp
+++ exam_login/WebContent/scoremanager/main/student_update_done.jsp
@@ -1,23 +1,18 @@
+<%-- ログアウトJSP --%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-
 <c:import url="/common/base.jsp">
-    <c:param name="title">
-    得点管理システム
-    </c:param>
+	<c:param name="title">
+		得点管理システム
+	</c:param>
 
 	<c:param name="content">
-		<div id="wrap-box">
-            <h2 class="h3 mb-3 fw-normal bg-secondary bg-opacity-10 py-2 px-4">学生情報更新</h2>
-            <div id="wrap-box">
-            	<p class="text-center" style="backround-color:#8cc3a9">更新が完了しました</p>
-
-            	<br>
-            	<br>
-            	<br>
-
-            	<a href="StudentList.action">学生一覧</a>
+		<div id="wrap_box">
+			<h2 class="h3 mb-3 fw-norma bg-secondary bg-opacity-10 py-2">学生情報変更</h2>
+			<div id="wrap_box">
+				<p class="text-center" style="background-color: #66CC99">変更が完了しました</p>
+				<a href="StudentList.action">学生一覧</a>
 			</div>
 		</div>
 	</c:param>
--- exam_login/src/scoremanager/main/StudentUpdateAction.java
+++ exam_login/src/scoremanager/main/StudentUpdateAction.java
@@ -13,38 +13,18 @@ import dao.StudentDao;
 import tool.Action;
 
 public class StudentUpdateAction extends Action {
-    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-
-    	HttpSession session = req.getSession();
-        Teacher teacher = (Teacher)session.getAttribute("user");
-
-        String no = "";
-        String name = "";
-		int entYear = 0;
-		String class_num = "";
-		boolean isAttend =false;
-		Student student = new Student();
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("user");
+		String no = req.getParameter("no");
 		StudentDao studentDao = new StudentDao();
-		ClassNumDao classNumDao = new ClassNumDao();
-
-		no = req.getParameter("no");
-		student = studentDao.get(no);
-		List<String> class_num_set = classNumDao.filter(teacher.getSchool());
-
-		entYear = student. getEntYear();
-		name = student. getName();
-		class_num = student. getClassNum();
-		isAttend = student. isAttend();
-
-		req. setAttribute("ent_year", entYear);
-		req. setAttribute("no", no);
-		req. setAttribute("name", name);
-		req. setAttribute("class_num", class_num);
-		req. setAttribute("class_num_set", class_num_set);
-		req. setAttribute("is_attend", isAttend);
-
-		// JSPへフォワード
-		req. getRequestDispatcher ("student_update.jsp"). forward (req, res);
-
-    }
+		Student student = studentDao.get(no);
+		ClassNumDao cNumDao = new ClassNumDao();
+		List<String> list = cNumDao.filter(teacher.getSchool());
+		req.setAttribute("student", student);
+		req.setAttribute("class_num_set", list);
+		req.setAttribute("is_attend", student.isAttend());
+		System.out.println(student.isAttend());
+		req.getRequestDispatcher("student_update.jsp").forward(req, res);
+	}
 }
