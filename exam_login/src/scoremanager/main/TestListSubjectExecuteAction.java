ff56246826ebd6582852f10f1aa7a821196c5e76
rion <tky2402017@stu.o-hara.ac.jp>
2025-04-25 12:02:44 +0900

    TestListSubjectExecuteAction.java

--- exam_login/src/scoremanager/main/TestListSubjectExecuteAction.java
+++ exam_login/src/scoremanager/main/TestListSubjectExecuteAction.java
@@ -2,7 +2,6 @@ package scoremanager.main;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,14 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.School;
-import bean.Student;
 import bean.Subject;
 import bean.Teacher;
-import bean.Test;
+import bean.TestListSubject;
 import dao.ClassNumDao;
-import dao.StudentDao;
 import dao.SubjectDao;
-import dao.TestDao;
+import dao.TestListSubjectDao;
 import tool.Action;
 
 public class TestListSubjectExecuteAction extends Action {
@@ -32,13 +29,11 @@ public class TestListSubjectExecuteAction extends Action {
 			String classNum = "";
 			String subjectCd = "";
 			int entYear = 0;
-			int idx = 0;
-			List<Student> students = null;
-			HashMap<Integer, Integer> points1 = new HashMap<>();
-			HashMap<Integer, Integer> points2 = new HashMap<>();
-			StudentDao sDao = new StudentDao();
-			TestDao tDao = new TestDao();
+
+			List<TestListSubject> tls = null;
+
 			SubjectDao sbDao = new SubjectDao();
+			TestListSubjectDao tlsDao = new TestListSubjectDao();
 	        LocalDate todayDate = LocalDate.now();
 	        int year = todayDate.getYear();
 
@@ -55,30 +50,20 @@ public class TestListSubjectExecuteAction extends Action {
 	        List<String> classNumSet = cNumDao.filter(teacher.getSchool());
 
 
+
 	        List<Subject> subjectSet = subjectDao.filter(teacher.getSchool());
 			entYearStr = req.getParameter("f1");
 			classNum = req.getParameter("f2");
 			subjectCd = req.getParameter("f3");
-			if (entYearStr != null) {
-				entYear = Integer.parseInt(entYearStr);
-			}
+
+
+	        Subject subject = null;
+	        subject = sbDao.get(subjectCd, school);
+
+
 			if (entYearStr != null && classNum != null && subjectCd != null) {
-				students = sDao.filter(school, entYear, classNum, false);
-				req.setAttribute("students", students);
-				for (Student s : students) {
-					Test test1 = null;
-					Test test2 = null;
-					test1 = tDao.get(s, sbDao.get(subjectCd, school), school, 1);
-					test2 = tDao.get(s, sbDao.get(subjectCd, school), school, 2);
-					if (test1 != null) {
-						points1.put(idx, test1.getPoint());
-					}
-					if (test2 != null) {
-						points2.put(idx, test2.getPoint());
-					}
-					idx++;
-				}
-			} else {
+				entYear = Integer.parseInt(entYearStr);
+				tls = tlsDao.filter(entYear, classNum, subject, school);
 
 			}
 			String subjectName = sbDao.get(subjectCd, school).getName();
@@ -87,8 +72,8 @@ public class TestListSubjectExecuteAction extends Action {
 			req.setAttribute("f2", classNum);
 			req.setAttribute("f3", subjectCd);
 			req.setAttribute("subjectName", subjectName);
-			req.setAttribute("points1", points1);
-			req.setAttribute("points2", points2);
+			req.setAttribute("tls", tls);
+
 	        req.setAttribute("ent_year_set", entYearSet);
 	        req.setAttribute("class_num_set", classNumSet);
 	        req.setAttribute("subject_set", subjectSet);
